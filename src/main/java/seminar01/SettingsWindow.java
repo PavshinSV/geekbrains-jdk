f7dadaa45367b12ae00b1c72357ea6fbc2142b5a
Pavshin Sergey <pavshin.s@cardiosib.ru>
2023-10-09 11:24:49 +0700

    добавил изменение состояний ярлыков для отображения текущего значения полей, и добавил проверку на соответствие длины линии размеру поля

--- src/main/java/seminar01/SettingsWindow.java
+++ src/main/java/seminar01/SettingsWindow.java
@@ -1,6 +1,8 @@
 package seminar01;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,19 +16,23 @@ public class SettingsWindow extends JFrame {
     private int gameLengthSize = 3;
     private int gameMode = 0;
 
+    private JSlider gameSizeSlider;
+    private JSlider lengthValueSlider;
+    private JLabel lengthSizeCurrentLabel;
+
     JButton btnStart = new JButton("Start new Game!!!");
 
-    SettingsWindow(GameWindow gameWindow){
+    SettingsWindow(GameWindow gameWindow) {
         this.gameWindow = gameWindow;
         setLocationRelativeTo(this.gameWindow);
-        setSize(WINDOW_WIDTH,WINDOW_HIGHT);
+        setSize(WINDOW_WIDTH, WINDOW_HIGHT);
 
         //Создаем панель для размещения надписей и кнопочек
         JPanel settingsPanel = new JPanel();
 
         //Создаем слой типа сетка с указанием того что все должно быть размещено в один столбец и с расстояниями между
         // элементами в 3 по вертикали и 3 по горизонтали
-        GridLayout layout = new GridLayout(0,1,3,0);
+        GridLayout layout = new GridLayout(0, 1, 3, 0);
         settingsPanel.setLayout(layout);
 
         //region ВЫБОР ТИПА ИГРЫ
@@ -52,7 +58,7 @@ public class SettingsWindow extends JFrame {
         gameTypeGroup.add(hThBtn);
         gameTypeGroup.add(hTcBtn);
         //Создадим панель для размещения кнопок в строку (я так хочу, я художник - я так вижу)
-        JPanel gameTypeGroupPanel = new JPanel(new GridLayout(1,0,3,0));
+        JPanel gameTypeGroupPanel = new JPanel(new GridLayout(1, 0, 3, 0));
         //Добавим кнопки на панель
         gameTypeGroupPanel.add(hThBtn);
         gameTypeGroupPanel.add(hTcBtn);
@@ -71,14 +77,14 @@ public class SettingsWindow extends JFrame {
         fieldSizeLabel.setFont(new Font("Verdana", Font.BOLD, 18));
         settingsPanel.add(fieldSizeLabel);
 
-        JLabel fieldSizeCurrent = new JLabel("Размер игрового поля: "+gameFieldSizeX+" x "+gameFieldSizeY);
+        JLabel fieldSizeCurrent = new JLabel("Размер игрового поля: " + gameFieldSizeX + " x " + gameFieldSizeY);
         //Разместим заголовок по центру
         fieldSizeCurrent.setHorizontalAlignment(SwingConstants.CENTER);
         //Зададим шрифт как радио-кнопкам, унификация :)
         fieldSizeCurrent.setFont(new Font("Verdana", Font.PLAIN, 10));
         settingsPanel.add(fieldSizeCurrent);
 
-        JSlider gameSizeSlider = new JSlider(3,10,3);
+        gameSizeSlider = new JSlider(3, 10, 3);
         settingsPanel.add(gameSizeSlider);
         gameFieldSizeX = gameSizeSlider.getValue();
         gameFieldSizeY = gameSizeSlider.getValue();
@@ -95,26 +101,59 @@ public class SettingsWindow extends JFrame {
         lengthValueLabel.setFont(new Font("Verdana", Font.BOLD, 18));
         settingsPanel.add(lengthValueLabel);
 
-        JLabel lengthSizeCurrentLabel = new JLabel("Длина линии: "+gameLengthSize);
+        lengthSizeCurrentLabel = new JLabel("Длина линии: " + gameLengthSize);
         //Разместим заголовок по центру
         lengthSizeCurrentLabel.setHorizontalAlignment(SwingConstants.CENTER);
         //Зададим шрифт как радио-кнопкам, унификация :)
         lengthSizeCurrentLabel.setFont(new Font("Verdana", Font.PLAIN, 10));
         settingsPanel.add(lengthSizeCurrentLabel);
 
-        JSlider lengthValueSlider = new JSlider(3,10,3);
+        lengthValueSlider = new JSlider(3, 10, 3);
         settingsPanel.add(lengthValueSlider);
         //endregion
 
         btnStart.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                gameWindow.startNewGame(gameMode,gameFieldSizeX,gameFieldSizeY,gameLengthSize);
+                gameWindow.startNewGame(gameMode, gameFieldSizeX, gameFieldSizeY, gameLengthSize);
                 setVisible(false);
             }
         });
+
+        gameSizeSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                gameFieldSizeX = gameSizeSlider.getValue();
+                gameFieldSizeY = gameSizeSlider.getValue();
+                fieldSizeCurrent.setText("Размер игрового поля: " + gameFieldSizeX + " x " + gameFieldSizeY);
+                checkLength();
+            }
+        });
+
+        lengthValueSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                gameLengthSize = lengthValueSlider.getValue();
+                checkLength();
+
+            }
+        });
+
+
         settingsPanel.add(btnStart);
 
         add(settingsPanel);
+    }
+
+    //Замутим проверку на соответствие длины линии размеру поля
+    private void checkLength(){
+        if (gameLengthSize>gameFieldSizeX){
+            gameLengthSize=gameFieldSizeX;
+        }
+        if (gameLengthSize>gameFieldSizeY){
+            gameLengthSize=gameFieldSizeY;
+        }
+        lengthValueSlider.setValue(gameLengthSize);
+        lengthSizeCurrentLabel.setText("Длина линии: " + gameLengthSize);
     }
 }
